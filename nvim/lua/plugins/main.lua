712176a9998bdb4e9634c58c022ea7f77439efb7
Hassan Qasemi <hassanak813@outlook.com>
2026-06-04 00:06:06 +0330

    smaller nvim

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "https://git.sr.ht/~swaits/scratch.nvim",
-    lazy = true,
-    keys = {
-      { "<leader>bs", "<cmd>Scratch<cr>", desc = "Scratch Buffer", mode = "n" },
-      { "<leader>bS", "<cmd>ScratchSplit<cr>", desc = "Scratch Buffer (split)", mode = "n" },
-    },
-    cmd = {
-      "Scratch",
-      "ScratchSplit",
-    },
-    opts = {},
-  },
-
-  {
     "OXY2DEV/markview.nvim",
     enabled = true,
     lazy = false, -- Recommended
@@ -22,42 +8,6 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
-    },
-  },
-
-  {
-    "ray-x/go.nvim",
-    dependencies = { -- optional packages
-      "ray-x/guihua.lua",
-      "neovim/nvim-lspconfig",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
-      -- lsp_keymaps = false,
-      -- other options
-    },
-    config = function(lp, opts)
-      require("go").setup(opts)
-      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-          require("go.format").goimports()
-        end,
-        group = format_sync_grp,
-      })
-    end,
-    event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
-
-  {
-    "benomahony/uv.nvim",
-    -- Optional filetype to lazy load when you open a python file
-    ft = { python },
-    opts = {
-      picker_integration = true,
     },
   },
 
